3abb8c1c7cd39a04a8754136a99d9536cb368ce8
AlvaroMF02 <alvaromarflo@gmail.com>
2023-11-22 11:49:16 +0100

    plantilla

--- REACT/ejercicios/ejercicio1/src/App.js
+++ REACT/ejercicios/ejercicio1/src/App.js
@@ -1,41 +1,33 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import { Button } from 'reactstrap';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: "danger",
+    }
+  }
+
+  cambia() {
+    if (this.state.color === "danger") {
+      this.setState({ color: "success" })
+    } else {
+      this.setState({ color: "danger" })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        {' '}
-        <Button color="success">
-          Inglés
+        <Button color={this.state.color} onClick={() => this.cambia()}>
+          Pulsa para cambiar de color.
         </Button>
-        {' '}
-
-        <Button color="primary">
-          Alemán
-        </Button>
-        {' '}
-
-        <Button color="secondary">
-          Sueco
-        </Button>
-        {' '}
-
-        <Button color="danger">
-          Español
-        </Button>
-        {' '}
-
-        <Button color="primary" outline>
-          Inglés
-        </Button>
-        {' '}
 
       </div>
     );
   }
-
 }
 
 export default App;
