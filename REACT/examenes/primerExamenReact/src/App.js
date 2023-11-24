34aad332990020db1f1efef7493d6aa60b14fbe0
AlvaroMF02 <alvaromarflo@gmail.com>
2023-11-24 11:13:32 +0100

    examen hecho sin array

--- REACT/examenes/primerExamenReact/src/App.js
+++ REACT/examenes/primerExamenReact/src/App.js
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'reactstrap';
 
 
+// hacerlo con array y pasando parametros para que no sea tan largo
+
 const Botoncillo = (props) => {
   return (
     // Da error diciendo que en color va un string pero le tengo que pasar una funcion
-    <Button onClick={() => props.funci()} /*color={() => props.colore()}*/ color={props.colore}>
-      {props.numero}
+    <Button onClick={() => props.funci()} color={props.colore}>
+    {props.numero}
     </Button>
   );
 }
@@ -32,64 +34,51 @@ class App extends Component {
 
   sumar1() {
     this.setState({ numero1: this.state.numero1 + 1 });
-  }
-  sumar2() {
-    this.setState({ numero2: this.state.numero2 + 1 });
-  }
-  sumar3() {
-    this.setState({ numero3: this.state.numero3 + 1 });
-  }
-  sumar4() {
-    this.setState({ numero4: this.state.numero4 + 1 });
-  }
-  sumar5() {
-    this.setState({ numero5: this.state.numero5 + 1 });
-  }
-
-  // no llega a entrar
-  color1() {
-    if (this.state.numero1 > 0) {
+    if (this.state.numero1 >= 0) {
       this.setState({ color1: "danger" })
     } else {
       this.setState({ color1: "secondary" })
     }
   }
-  color2() {
-    if (this.state.numero2 > 0) {
+  sumar2() {
+    this.setState({ numero2: this.state.numero2 + 1 });
+    if (this.state.numero2 >= 0) {
       this.setState({ color2: "danger" })
     } else {
       this.setState({ color2: "secondary" })
     }
   }
-  color3() {
-    if (this.state.numero3 > 0) {
+  sumar3() {
+    this.setState({ numero3: this.state.numero3 + 1 });
+    if (this.state.numero3 >= 0) {
       this.setState({ color3: "danger" })
     } else {
       this.setState({ color3: "secondary" })
     }
   }
-  color4() {
-    if (this.state.numero4 > 0) {
+  sumar4() {
+    this.setState({ numero4: this.state.numero4 + 1 });
+    if (this.state.numero4 >= 0) {
       this.setState({ color4: "danger" })
     } else {
       this.setState({ color4: "secondary" })
     }
   }
-  color5() {
-    if (this.state.numero5 > 0) {
+  sumar5() {
+    this.setState({ numero5: this.state.numero5 + 1 });
+    if (this.state.numero5 >= 0) {
       this.setState({ color5: "danger" })
     } else {
       this.setState({ color5: "secondary" })
     }
   }
 
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
 
-          <Botoncillo numero={this.state.numero1} funci={() => this.sumar1()} /*colore={() => this.color1()}*/ />
+          <Botoncillo numero={this.state.numero1} funci={() => this.sumar1()} colore={this.state.color1} />
           {" "}
           <Botoncillo numero={this.state.numero2} funci={() => this.sumar2()} colore={this.state.color2} />
           {" "}
@@ -105,3 +94,4 @@ class App extends Component {
 }
 
 export default App;
+
