e554db917782fbdec978b5856259061bed40965e
alvaro <alvaromarflo@gmail.com>
2023-11-28 20:09:17 +0100

    esta tu no las tienes

---
+++ REACT/apps/minevancic/src/componentes/FlashcardComponent.js
@@ -0,0 +1,14 @@
+import { Card, CardImg, CardBody, CardTitle,CardText } from 'reactstrap';
+import { Spinner, Button } from 'reactstrap';
+
+
+const Tablero = (props) => {
+  // devuelve solo uno
+    return (<>
+      <Button>
+        
+      </Button>
+    </>);
+  }
+
+export default Flashcard;
