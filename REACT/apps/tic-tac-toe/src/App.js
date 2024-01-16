9479eb9d3c416714a7a9a5619c53fde57256c382
alvaro <alvaromarflo@gmail.com>
2024-01-16 18:53:40 +0100

    acabado pocho

--- REACT/apps/tic-tac-toe/src/App.js
+++ REACT/apps/tic-tac-toe/src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Button } from 'reactstrap';
 import './App.css';
 import { Caja } from './Componentes/Caja.js';
@@ -7,12 +7,62 @@ import { Tablero } from './Componentes/Tablero.js';
 
 function App() {
 
-  // minuto 15:17 cuando importa para hacer el hook
-  const tablero = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+  // Condiciones en las que se gana a lo bruto
+  const jugadas = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 1, 6]
+  ]
+
+  // Tablero
+  const [tablero, setTablero] = useState(Array(9).fill(null))
+  // Turno X
+  const [turnoX, setTurnoX] = useState(true)
+  // Ganador
+  const [ganador, setGanador] = useState("")
+
+
+
+  // se ejecuta al hacer click en una caja
+  const handleClick = (cajaId) => {
+    // copia del tablero, si el id de la caja es igual al indice del 
+    // tablero, se devuelve una X
+    let auxTablero = tablero.map((valor, indice) => {
+      if (indice == cajaId) {
+        return turnoX === true ? "X" : "O"
+      } else {
+        return valor
+      }
+    })
+    // comprobar el ganador
+    setGanador(comprobarGanador(auxTablero))
+
+    // actualizamos el tablero con la copia
+    setTablero(auxTablero)
+    // va cambiando cada vez que se da un click
+    setTurnoX(!turnoX)
+  }
+
+  const comprobarGanador = (tablero) => {
+    for (let i = 0; i < jugadas.length; i++) {
+      
+      let [x, y, z] = jugadas[i]
+
+      if (tablero[x] && tablero[x] === tablero[y] && tablero[y] === tablero[z]) {
+        return tablero[x]
+      }
+    }
+  }
 
   return (
     <div className="App">
-      <Tablero tablero={tablero} onClick={null}/>
+      <Tablero tablero={tablero} onClick={handleClick} />
+      Ha ganado: {ganador}
     </div>
   );
 
--- REACT/apps/tic-tac-toe/src/Componentes/Tablero.js
+++ REACT/apps/tic-tac-toe/src/Componentes/Tablero.js
@@ -5,8 +5,9 @@ import './Tablero.css'
 export const Tablero = ({ tablero, onClick }) => {
     return (
         <div className='tablero'>
+            {/* Recorre el tablero devolviendo cajas con el index q se crea al hacer el map */}
             {tablero.map((valor, index) => {
-                return <Caja valor={valor} onClick={() => onClick(index)} />
+                return <Caja valor={valor} onClick={() => valor === null && onClick(index)} />
             })}
         </div>
     )
