74387cb9556b0b515b0eea429c3b3cfd6f2cb813
AlvaroMF02 <alvaromarflo@gmail.com>
2024-01-18 12:51:29 +0100

    miau

--- REACT/ROOTING/rutas/src/App.js
+++ REACT/ROOTING/rutas/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import {Outlet,Link} from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
-function App() {
+function App () {
   return (
     <div className="App">
-     <h1>Bienvenido a la APP</h1>
+      <h1>Bienvenido a la APP</h1>
 
-     <nav>
+      <nav>
         {/* Dependiendo del to cambia el Outlet configurado en el index.js */}
         <Link to="/">Home</Link> {" "}
         <Link to="/Reservas">Reservas</Link> {" "}
         <Link to="/Administracion">Administracion</Link> {" "}
         <Link to="/Usuario">Usuario</Link> {" "}
-     </nav>
+      </nav>
 
-     <Outlet/>
+      <Outlet />
 
     </div>
   );
